bccba89ad1fc1e1f9c384b8a19d33dd46f657a58
minhle10 <lems@bc.edu>
2022-06-09 07:41:23 -0400

    cache whether user is in dark mode

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,7 +3,12 @@ import React, {createContext, useState, useEffect} from "react";
 export const ThemeContext = React.createContext();
 
 export default function ThemeContextProvider(props){
-    const [theme, setTheme] = useState('light')
+    const [theme, setTheme] = useState(JSON.parse(window.localStorage.getItem('theme')) || 'light')
+
+    useEffect(() => {
+        window.localStorage.setItem("theme", JSON.stringify(theme));
+      }, [theme]);
+
     return <ThemeContext.Provider value = {{
         theme,
         setTheme
