f36fa3c1bed45173edf8004d0bf1ea68e159b4be
minhle10 <lems@bc.edu>
2022-07-22 18:00:46 -0400

    create tool tip component

---
+++ src/components/ToolTip.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import styled, { css } from "styled-components";
+
+export default function ToolTip({toolTip, toolTipText}) {
+    return <Text> {toolTip}
+        <ToolTipText>{toolTipText}</ToolTipText>
+    </Text>;
+}
+
+const Text = styled.div`
+    position: relative;
+    display: inline-block;
+    border-bottom: 2px dotted white;
+`;
+const ToolTipText = styled.span`
+    visibility: hidden;
+    font-size: 10px;
+    width: 200px;
+    background-color: #0d1227;
+    color: #fff;
+    text-align: center;
+    border-radius: 10px;
+    border: 1px solid white;
+    padding: 10px 0;
+    margin-left: 30px;
+    bottom: 100%;
+
+    position: absolute;
+    z-index: 1;
+    ${Text}:hover & {
+        visibility: visible;
+    }
+`;
