67f473828c21f2db6ae8f480fe9cdc71928a37f2
Esme Y <61486616+esyeng@users.noreply.github.com>
2022-07-24 15:41:41 -0400

    dynamic page toggle complete, hide pool btn

--- src/components/swap/PageToggle.jsx
+++ src/components/swap/PageToggle.jsx
@@ -1,25 +1,60 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 
-export default function PageToggle({ pages }) {
+export default function PageToggle({ selectedTab }) {
+const [swap, setSwap] = useState(true)
+const [pool, setPool] = useState(false)
   return (
     <div
       style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "20vh",
-        width: "100vh",
-
+        height: "6vh",
+        width: "20vh",
+        background: "#000000",
+        borderRadius: "12px"
       }}
     >
-        <Text>Swap</Text>
-        <Text>Pool</Text>
+        <Btn
+        selected={swap}
+        onClick={() => {
+          selectedTab("swap")
+          if (swap !== true) {
+            setSwap(true)
+            setPool(false)
+          }
+        }} >Swap</Btn>
+        {/* <Btn
+        selected={pool}
+        onClick={() => {
+          selectedTab("pool")
+          if (pool !== true) {
+            setSwap(false)
+            setPool(true)
+          }
+        }}
+        >Pool</Btn> */}
     </div>
   );
 }
 
 
-const Text = styled.text`
-    /*  */
+const Btn = styled.text`
+  border: 1px transparent;
+  height: 30px;
+  width: 60px;
+  text-align: center;
+  border-radius: 4px;
+  ${(props) =>
+    props.selected &&
+    css`
+      background: #172756;
+      text-decoration: underline;
+      text-decoration-style: dotted;
+    `
+  }
+  &:hover {
+    border: 1px solid #019fff;
+  }
 `
