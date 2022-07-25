0cbc7668f512672c0eb1313c4757142301159b1d
Esme Y <61486616+esyeng@users.noreply.github.com>
2022-07-24 23:57:28 -0400

    initial commit for pool details

---
+++ src/components/pool/PoolDetails.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+
+
+export default function PoolDetails() {
+    return(
+        <div>Pool</div>
+    )
+}
