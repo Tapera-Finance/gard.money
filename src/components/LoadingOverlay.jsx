07f4f06ed0eaf8c84c7f2eb4d5ab79539838f547
minhle10 <lems@bc.edu>
2022-09-12 08:55:58 -0400

    loading overlay progress + close button

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -1,11 +1,25 @@
 import React, { useReducer, useState, useContext } from "react";
 import styled, { keyframes, css } from "styled-components";
+import puppy from "../assets/puppy-min.png"
+import closeIcon from "../assets/icons/close_icon.png";
 
-export default function LoadingOverlay({ text }) {
+
+export default function LoadingOverlay({ text, close }) {
   return (
     <div>
       <Backdrop>
         <TextContainer>
+          <CloseButton onClick={() => close()}>
+              <img style={{height: 20}} src={closeIcon} />
+          </CloseButton>
+          {/* <img 
+          style={{
+            height: "40%",
+            objectFit: "cover",
+            borderRadius: 10,
+          }} 
+          src={puppy} 
+          alt="puppy"/> */}
           <div>
             <LoadingText>{text || "Loading..."}</LoadingText>
           </div>
@@ -22,8 +36,8 @@ const TextAnimation = keyframes`
 `;
 
 const LoadingText = styled.text`
-  font-weight: 900;
-  font-size: 24px;
+  font-weight: 500;
+  font-size: 14px;
   animation-name: ${TextAnimation};
   animation-duration: 1s;
   animation-iteration-count: infinite;
@@ -33,22 +47,28 @@ const LoadingText = styled.text`
 
 const Backdrop = styled.div`
   position: fixed;
-  height: 100vh;
-  width: 100vw;
-  left: 0;
-  top: 0;
+  right: 30px;
+  bottom: 30px;
   z-index: ${21};
-  background: ${"#b0b0b080"};
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 const TextContainer = styled.div`
-  height: 100px;
-  background: rgba(13, 18, 39);
+  height: 130px;
+  width: 400px;
+  border: 1px solid white;
+  background: #0E1834;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-radius: 25px;
-  padding: 0px 40px;
+  border-radius: 10px;
+  padding: 0px 20px 40px 20px;
+`;
+const CloseButton = styled.button`
+  position: relative;
+  bottom: 30px;
+  left: 15px;
+  border: 0px;
+  background: transparent;
+  cursor: pointer;
+  align-self: end;
 `;
