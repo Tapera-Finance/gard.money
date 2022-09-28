60ef62ee0fe21bf6bc0a566dcbaf4a74d7ccc622
Eliana Troper <eliana@troper.report>
2022-09-28 14:58:43 -0400

    First pass at unstake

--- src/transactions/stake.js
+++ src/transactions/stake.js
@@ -52,6 +52,48 @@ export async function stake(pool, gardAmount) {
   return response;
 }
 
-export async function unstake(pool, amount) {
+export async function unstake(pool, gardAmount) {
+  setLoadingStage("Loading...");
+
+  let infoPromise = accountInfo();
+  let microGARDAmount = microGARD(gardAmount);
+
+  // XXX: This could be more optimally set -
+  //      for locked pools if it's not a valid
+  //      withdrawal period, only needs to be 1000
+  let params = await getParams(2000);
+  let info = await infoPromise;
+  
+  // txn 0 - app call
+  params.fee = 0
+  let txn1 = algosdk.makeApplicationCallTxnFromObject({
+    from: info.address,
+    appIndex: ids.app.gard_staking,
+    onComplete: 0,
+    appArgs: [enc.encode("exit_" + pool + "_pool"), algosdk.encodeUint64(microGARDAmount)],
+    accounts: [],
+    foreignApps: [],
+    foreignAssets: [ids.asa.gard], // XXX: When we do NLL, this will have to change
+    suggestedParams: params,
+  });
+  
+
+  let txns = [txn0];
+  algosdk.assignGroupID(txns);
+  
+  setLoadingStage("Awaiting Signature from Algorand Wallet...");
+  const signedGroup = await signGroup(info, txns);
+
+  setLoadingStage("Confirming Transaction...");
+
+  let stxns = [signedGroup[0].blob];
+
+  let response = await sendTxn(
+    stxns,
+    "Successfully unstaked " + gardAmount + " GARD.", // TODO: if unstaking all, display that amount
+  );
+  setLoadingStage(null);
+
+  return response;
   // TODO
 }
