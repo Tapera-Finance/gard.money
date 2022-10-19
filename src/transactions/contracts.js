f6c6f0fd425d74f5fa91f91ca3d20eee7898fa99
Eliana Troper <eliana@troper.report>
2022-10-19 13:59:14 -0400

    Removed comment

--- src/transactions/contracts.js
+++ src/transactions/contracts.js
@@ -62,7 +62,7 @@ export function cdpGen(userAddress, accountID, asaID = 0) {
   if (asaID == 0) {
     const sub1 = template.slice(slices[0], slices[1]);
     const sub2 = template.slice(slices[2], slices[3]);
-    const sub3 = template.slice(slices[4]); // TODO: Check that slicing is done in the same way
+    const sub3 = template.slice(slices[4]);
 
     // Crafts the contract with proper insertions
     contract = new Uint8Array([
