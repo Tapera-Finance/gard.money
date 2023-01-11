8b9062a1796ea3d80695c3ec984ca9b92b828494
David McCabe <43038510+mccabe-david@users.noreply.github.com>
2023-01-11 15:01:42 -0500

    Count GARDIAN staking as users

--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -75,7 +75,7 @@ async function getTotalUsers() {
   let response = null;
   const users = new Set();
 
-  const validators = [ids.app.validator, ids.app.gard_staking]
+  const validators = [ids.app.validator, ids.app.gard_staking, ids.app.gardian_staking]
   for(var i = 0; i < validators.length; i++){
     do {
       // Find accounts that are opted into the GARD price validator application
