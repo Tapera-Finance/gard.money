bcf5015e215601c685a13fdfad82c66c6ff402bd
minhle10 <lems@bc.edu>
2023-01-18 17:13:27 -0500

    revert to old underline

--- src/components/ToolTip.jsx
+++ src/components/ToolTip.jsx
@@ -16,8 +16,6 @@ const Text = styled.div`
     display: inline-block;
     cursor: help;
     text-align: center;
-    text-decoration: underline;
-    text-decoration-style: dashed;
 `;
 const ToolTipText = styled.span`
   visibility: hidden;
